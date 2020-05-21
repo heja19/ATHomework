b415af95aaf35bd093f45851dceb26245d9f5a14
Helena Jäe <56818449+helenajae@users.noreply.github.com>
2020-05-21 22:34:09 +0300

    small changes

--- HW2/src/test/java/data/CourseTest.java
+++ HW2/src/test/java/data/CourseTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import school.Course;
 import services.PublicHolidayService;
 
 import java.time.ZonedDateTime;
@@ -16,7 +17,7 @@ import static org.mockito.Mockito.when;
 public class CourseTest {
     ZonedDateTime startDate = ZonedDateTime.parse("2020-05-01T00:00:00.000+00:00[UTC]");
     ZonedDateTime endDate = ZonedDateTime.parse("2020-04-01T00:00:00.000+00:00[UTC]");
-    ZonedDateTime dateOfBirth = ZonedDateTime.parse("2020-01-01T00:00:00.000+00:00[UTC]");
+    //ZonedDateTime dateOfBirth = ZonedDateTime.parse("2020-01-01T00:00:00.000+00:00[UTC]");
 
     @Mock
     private PublicHolidayService publicHolidayService;
